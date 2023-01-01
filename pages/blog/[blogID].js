4bcae72df4f93f73940ca3a0583efd6324c4e67a
Praveen Krishna <praveenkrishna0512@gmail.com>
2023-01-01 19:30:11 +0800

    shifted blog content entirely to mongodb

--- pages/blog/[blogID].js
+++ pages/blog/[blogID].js
@@ -1,8 +1,5 @@
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-import fetcher from "../../lib/fetcher";
+import React from "react";
 import { SWRConfig } from "swr";
-import getBlog from "../../lib/helper";
 import classes from "./blog.module.css";
 import clientPromise from "../../lib/mongodb";
 
@@ -15,96 +12,48 @@ export default function BlogPage({ fallback, data }) {
 }
 
 function BlogContent({ id, title, author, datePosted, content }) {
-
   if (!content) {
     return (
       <div className={classes.wrapper}>
-      <div className={classes.mainContent}>
-        <h1>This blog page does not exist</h1>
-        <h5>Please check the URL</h5>
+        <div className={classes.mainContent}>
+          <h1>This blog page does not exist</h1>
+          <h5>Please check the URL</h5>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
-  
+
   return (
     <div className={classes.wrapper}>
       <div className={classes.mainContent}>
         <h1>{title}</h1>
         <h5>Written by: {author}</h5>
         <h5>{datePosted}</h5>
-        <p className={classes.body}>{`${content}`}</p>
+        <p className={classes.body}>
+          {content.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+          ))}
+        </p>
       </div>
     </div>
   );
 }
 
-// export async function getStaticProps({ params }) {
-//   // const req = {
-//   //   method: "GET",
-//   //   query: params.blogID,
-//   //   headers: {
-//   //     "Content-Type": "application/json",
-//   //   },
-//   // };
-//   const blog = await getBlog(params.blogID);
-//   // const { data } = await handler(req, {})
-
-
-//   return {
-//     props: {
-//       fallback: {
-//         "/api/blogs": blog,
-//       },
-//     },
-//   };
-// }
-
-// export async function getStaticPaths() {
-//   // const req = {
-//   //   method: "GET",
-//   //   headers: {
-//   //     "Content-Type": "application/json",
-//   //   },
-//   // };
-//   const blogs = await getBlog();
-//   // const { data } = await handler(req, {})
-
-//   const paths = blogs.map((value) => {
-//     return {
-//       params: {
-//         blogID: value.id,
-//       },
-//     };
-//   });
-
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// }
-
 export async function getServerSideProps({ params }) {
-  // const req = {
-  //   method: "GET",
-  //   query: params.blogID,
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  // };
-  // const blog = await getBlog(params.blogID);
-  // const { data } = await handler(req, {})
 
   const client = await clientPromise;
   const db = client.db("personal-website");
 
-  const data = await db.collection("blogs").find({id: params.blogID}).limit(20).toArray();
+  const data = await db
+    .collection("blogs")
+    .find({ id: params.blogID })
+    .toArray();
 
-  let blog
+  let blog;
   if (data.length != 0) {
-    blog = JSON.parse(JSON.stringify(data))[0]
+    blog = JSON.parse(JSON.stringify(data))[0];
   } else {
-    blog = {}
+    blog = {};
   }
 
   return {
@@ -112,7 +61,7 @@ export async function getServerSideProps({ params }) {
       fallback: {
         "/api/blogs": blog,
       },
-      data: blog
+      data: blog,
     },
   };
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,15 +1,43 @@
 import classes from "./blogListPage.module.css";
 import BlogListView from "../../components/blog/blogListView";
 import { useState, useEffect } from "react";
+import clientPromise from "../../lib/mongodb";
 
-function BlogListPage() {
+function BlogListPage({ fallback, data }) {
 
   return (
     <div className={classes.wrapper}>
       <h1>My Blog</h1>
-      <BlogListView/>
+      <BlogListView blogs={data}/>
     </div>
   );
+}
+
+export async function getServerSideProps({ params }) {
+
+  const client = await clientPromise;
+  const db = client.db("personal-website");
+
+  const data = await db
+    .collection("blogs")
+    .find({})
+    .toArray();
+
+  let blogs;
+  if (data.length != 0) {
+    blogs = JSON.parse(JSON.stringify(data));
+  } else {
+    blogs = {};
+  }
+
+  return {
+    props: {
+      fallback: {
+        "/api/blogs": blogs,
+      },
+      data: blogs,
+    },
+  };
 }
 
 export default BlogListPage;
